f7e000f769039b138260a8d8f338747194e6e3ca
muskan garg <muskangarg02270@gmail.com>
2023-08-14 14:48:26 +0530

    photo upload

--- client/app/components/UploadPhoto.jsx
+++ client/app/components/UploadPhoto.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+"use client";
+import React, { useState, useRef } from 'react';
 import Image from "next/image";
 import Avatar from "../../public/user.png";
 
 const UploadPhoto = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+    const fileInputRef = useRef(null);
+
+    function handleImageChange(event) {
+        const selectedFile = event.target.files[0];
+        setSelectedImage(URL.createObjectURL(selectedFile));
+    }
+
+    function handleUploadButtonClick() {
+        fileInputRef.current.click();
+    }
+
     return (
         <div className="sm:flex text-center sm:justify-between mb-2 p-5 pt-2">
             <div className="flex justify-center">
-                <Image src={Avatar} alt="Avatar" className="w-28 bg-cover" />
+                {selectedImage ? (
+                    <img src={selectedImage} alt="Avatar" className="bg-cover w-28 h-28 rounded-full" />
+                ) : (
+                    <Image src={Avatar} alt="Avatar" className="w-28 bg-cover" />
+                )}
             </div>
-            <div>
-                <button className="smoky-btn sm:mt-[40px] mt-5">Upload Photo</button>
+            <div className="sm:mt-[40px] mt-5">
+                <label className="smoky-btn cursor-pointer">
+                    Upload Photo
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        ref={fileInputRef}
+                        className="hidden"
+                    />
+                </label>
             </div>
         </div>
-    )
+    );
 }
 
 export default UploadPhoto;
+
